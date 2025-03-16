79440104477af13d20f7b3b2600783c8e48ebefc
IsidoriM <isidorimassimiliano2@gmail.com>
2025-03-16 17:03:35 +0100

    Lesson 1 WebApi

---
+++ WebApplication1/Controllers/FirstController1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class FirstController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Hello World";
+            return View();
+        }
+        public IActionResult Info()
+        {
+            Person person = new Person();
+            person.Name = "John";
+            person.Age = 18;
+            person.Location = "United States";
+            return View(person);
+        }
+    }
+
+}
